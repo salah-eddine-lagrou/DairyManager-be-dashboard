e8a345e94fcc68c984f3e53ba4353a06deb5b9fb
Lagrou Salah <lagrousalah04@gmail.com>
2024-08-27 10:45:46 +0200

    creates empty tables and models

---
+++ app/Models/PayementTerms.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PayementTerms extends Model
+{
+    use HasFactory;
+}
